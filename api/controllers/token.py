b35ab631a2ad23d71a839b21e239bceddb4ebc80
Akuzike8 <akuzikenchembe8@gmail.com>
2023-09-09 10:45:05 +0200

    JWT structure constructed

---
+++ api/controllers/token.py
@@ -0,0 +1,13 @@
+
+class Token:
+    def generate_token():
+        return {'message':'JWT generated'}
+
+    def verify_token():
+        return {'message':'verified'}
+
+    def generate_csrf_token():
+        return {'message':'csrf token generated'}
+
+    def verify_csrf_token():
+        return {'message':'csrf token verified'}
